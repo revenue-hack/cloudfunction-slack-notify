c90426a35593596eb03a50579834c6a48f9112a9
Kohei Ozeki <K.odeveloper10@gmail.com>
2018-11-23 10:53:58 +0900

    [add] all read notifications after send to slack

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
+
 require('dotenv').config();
+const request = require('request');
+const sprintf = require('sprintf-js').sprintf;
+const GitHub = require('github-api');
 
 exports.http = (request, response) => {
-  const request = require('request');
-  const sprintf = require('sprintf-js').sprintf;
-  const GitHub = require('github-api');
   const gh = new GitHub({
     token: process.env.GH_TOKEN
   });
@@ -13,28 +14,47 @@ exports.http = (request, response) => {
   me.listNotifications((err, notifications) => {
     if (err) {
       console.log(err);
+      response.status(500).send('fail api github api error');
     }
     var bodyMessage = '';
     notifications.forEach(e => {
-      if (!e.repository.private) {
+      if (e.unread && !e.repository.private) {
         bodyMessage += sprintf("%s[%s]: %s %s\n\n",
             e.updated_at,
             e.repository.name,
             e.subject.title,
-            e.subject.url);
+            e.repository.html_url);
       }
     });
     sendToSlack(bodyMessage);
+    allReads();
   });
-  response.status(200).send('Hello World!');
+  response.status(200).send('ok');
 };
 
+const allReads = () => {
+  request.put({
+    uri: process.env.GH_API_URI + '/notifications',
+    headers: {
+      'User-Agent': process.env.GH_API_URI,
+      'Authorization': 'token ' + process.env.GH_TOKEN
+    }
+  }, (error, res, body) => {
+    if (!error && res.statusCode === 205) {
+      console.log(body);
+    } else {
+      console.log(error);
+      response.status(500).send('fail all reads');
+    }
+  });
+};
 
-sendToSlack = (bodyMessage) => {
+const sendToSlack = (bodyMessage) => {
   request.post({
     uri: process.env.INCOME_URI,
     headers: { 'Content-Type': 'application/json' },
     json: {
+      channel: '#oss',
       username: 'OSSチェッカー',
       icon_emoji: ':ghost:',
       text: bodyMessage
@@ -43,7 +63,13 @@ sendToSlack = (bodyMessage) => {
     if (!error && response.statusCode === 200) {
       console.log(body);
     } else {
-      console.log('error');
+      console.log(error);
+      response.status(500).send('fail send to slack');
     }
   });
 };
+
+exports.event = (event, callback) => {
+  callback();
+};
+
